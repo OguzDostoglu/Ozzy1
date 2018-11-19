e0cc5405ba83eb0bc418ecc10856fccf9651620f
Oguz <Ozzy@ozzys-macbook-pro.local>
2018-11-18 23:40:34 -0600

    Initial Commit

---
+++ Ozzy1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ozzy1
+//
+//  Created by Ozzy on 11/18/18.
+//  Copyright © 2018 Ozzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
